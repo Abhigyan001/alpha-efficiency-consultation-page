e74570e56ef237e63bfb6e1ae67c4119c9107053
Abhigyan001 <abhigyanmahanta001@gmail.com>
2023-08-13 14:01:32 +0530

    Implement hamburger menu

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
+// Nav menu
+const hamburger = document.querySelector("#hamburger-menu");
+const navMenu = document.querySelector(".nav-menu-links");
+const navLink = document.querySelectorAll(".nav-link");
+const closeIcon = document.querySelector("#close-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+closeIcon.addEventListener("click", closeMenu);
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+  hamburger.classList.add("hidden");
+  navMenu.classList.toggle("active");
+  closeIcon.classList.remove("hidden");
+}
+
+function closeMenu() {
+  hamburger.classList.remove("hidden");
+  closeIcon.classList.add("hidden");
+  navMenu.classList.remove("active");
+}
+
 // Coutdown timer
-const countTime = new Date(2023, 7, 13, 0, 0, 0).getTime();
+const countTime = new Date(2023, 8, 13, 0, 0, 0).getTime();
 const days = document.querySelector("#days");
 const hours = document.querySelector("#hours");
 const minutes = document.querySelector("#minutes");
 const seconds = document.querySelector("#seconds");
 
 let countDown = setInterval(function () {
-  const dateTimeNow = new Date();
+  const dateTimeNow = new Date().getTime();
   let difference = countTime - dateTimeNow;
 
   days.innerHTML = Math.floor(difference / (1000 * 60 * 60 * 24));
