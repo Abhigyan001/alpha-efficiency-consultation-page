0ec5146a8436f19bd22206624a1c6d22e808ea2e
Abhigyan001 <abhigyanmahanta001@gmail.com>
2023-08-13 13:03:25 +0530

    Implement coutdown functionality

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
+// Coutdown timer
+const countTime = new Date(2023, 7, 13, 0, 0, 0).getTime();
+const days = document.querySelector("#days");
+const hours = document.querySelector("#hours");
+const minutes = document.querySelector("#minutes");
+const seconds = document.querySelector("#seconds");
+
+let countDown = setInterval(function () {
+  const dateTimeNow = new Date();
+  let difference = countTime - dateTimeNow;
+
+  days.innerHTML = Math.floor(difference / (1000 * 60 * 60 * 24));
+  hours.innerHTML = Math.floor(
+    (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  minutes.innerHTML = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  seconds.innerHTML = Math.floor((difference % (1000 * 60)) / 1000);
+
+  if (difference < 0) {
+    clearInterval(countDown);
+  }
+}, 1000);
+
 // FAQ Toggle
 
 const faqs = document.querySelectorAll(".faq");
